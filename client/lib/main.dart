bdffbdaf2a35f5bd6e59d24e3d021d6d8a605d44
Ivan Nikitsin <ivan@nikitin.by>
2025-06-19 21:18:37 +0300

    Add road quality monitoring features with GPS and accelerometer data collection

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,4 +1,13 @@
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'package:permission_handler/permission_handler.dart';
+
+// URL of your backend. Replace with the IP or add of your server.
+const String backendUrl = 'http://nikitin.by/roader/api/ingest/';
 
 void main() {
   runApp(const MyApp());
@@ -7,116 +16,167 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Road Quality Monitor',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  bool _isRecording = false;
+  StreamSubscription? _accelerometerSubscription;
+  StreamSubscription? _positionSubscription;
+  
+  final List<Map<String, dynamic>> _dataBuffer =;
+  Timer? _uploadTimer;
 
-  void _incrementCounter() {
+  Position? _currentPosition;
+  UserAccelerometerEvent? _latestAccelerometerEvent;
+
+  @override
+  void initState() {
+    super.initState();
+    _requestPermissions();
+  }
+
+  Future<void> _requestPermissions() async {
+    await Permission.location.request();
+    await Permission.sensors.request();
+  }
+
+  void _toggleRecording() {
+    if (_isRecording) {
+      _stopRecording();
+    } else {
+      _startRecording();
+    }
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _isRecording =!_isRecording;
     });
+  }
+
+  void _startRecording() {
+    // Starting the GPS
+    _positionSubscription = Geolocator.getPositionStream(
+      locationSettings: const LocationSettings(
+        accuracy: LocationAccuracy.high,
+        distanceFilter: 1, // Update with 1 meter interval.
+      ),
+    ).listen((Position position) {
+      setState(() {
+        _currentPosition = position;
+      });
+    });
+
+    // Starting the accelerometer
+    _accelerometerSubscription = userAccelerometerEvents.listen(
+      (UserAccelerometerEvent event) {
+        _latestAccelerometerEvent = event;
+        if (_currentPosition!= null) {
+          _collectDataPoint(event, _currentPosition!);
+        }
+      },
+    );
+
+    // Starting the upload timer
+    _uploadTimer = Timer.periodic(const Duration(seconds: 10), (timer) {
+      _uploadData();
+    });
+  }
+
+  void _stopRecording() {
+    _accelerometerSubscription?.cancel();
+    _positionSubscription?.cancel();
+    _uploadTimer?.cancel();
+    _uploadData(); // Upload any remaining data points
+    setState(() {
+      _currentPosition = null;
+      _latestAccelerometerEvent = null;
+    });
+  }
+
+  void _collectDataPoint(UserAccelerometerEvent accEvent, Position pos) {
+    final dataPoint = {
+      "timestamp": DateTime.now().toIso8601String(),
+      "gps": {
+        "latitude": pos.latitude,
+        "longitude": pos.longitude,
+        "speed": pos.speed,
+        "accuracy": pos.accuracy,
+      },
+      "user_accelerometer": {
+        "x": accEvent.x,
+        "y": accEvent.y,
+        "z": accEvent.z,
+      },
+    };
+    _dataBuffer.add(dataPoint);
+  }
+
+  Future<void> _uploadData() async {
+    if (_dataBuffer.isEmpty) return;
+
+    final List<Map<String, dynamic>> dataToSend = List.from(_dataBuffer);
+    _dataBuffer.clear();
+
+    try {
+      final response = await http.post(
+        Uri.parse(backendUrl),
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode(dataToSend),
+      );
+
+      if (response.statusCode == 202) {
+        print('Successfully uploaded ${dataToSend.length} data points.');
+      } else {
+        print('Failed to upload data. Status code: ${response.statusCode}');
+        print('Response body: ${response.body}');
+        // Re-add the data to the buffer if upload fails
+        _dataBuffer.addAll(dataToSend);
+      }
+    } catch (e) {
+      print('Error uploading data: $e');
+      _dataBuffer.addAll(dataToSend);
+    }
+  }
+
+  @override
+  void dispose() {
+    _stopRecording();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Road Quality Monitor'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          children: <Widget>,
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _toggleRecording,
+        tooltip: 'Toggle Recording',
+        child: Icon(_isRecording? Icons.stop : Icons.play_arrow),
+      ),
     );
   }
 }
