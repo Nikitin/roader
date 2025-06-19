1284940cd798f771531bad20a43a1f02ef79bea7
Ivan Nikitsin <ivan@nikitin.by>
2025-06-19 22:36:49 +0300

    Add Django template configuration to settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,3 +129,19 @@ CELERY_RESULT_BACKEND = 'redis://redis:6379/0' # 'redis' - thats the name of  se
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
