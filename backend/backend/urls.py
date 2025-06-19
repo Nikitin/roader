5f88945b8426cefb2adc3c463a112f47e04fd414
Ivan Nikitsin <ivan@nikitin.by>
2025-06-19 23:19:10 +0300

    Add additional URL patterns for admin and API endpoints in urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('roader/admin/', admin.site.urls),
     path('roader/api/', include('api.urls')),
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
 ]
